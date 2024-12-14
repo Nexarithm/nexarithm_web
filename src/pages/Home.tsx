badfe71e833b5ec82e9ff9af0723e988e8db706f
proxai <github@proxai.co>
2024-12-14 15:39:45 -0500

    bug: typescript error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,3 @@
-import {
-  SocialLink,
-  TwitterIcon,
-  BlueskyIcon,
-  MediumIcon,
-  SubstackIcon,
-  HackerNewsIcon,
-  RedditIcon,
-  GitHubIcon,
-  EmailIcon,
-} from '../components/SocialLinks';
 import { AboutSection } from './About';
 
 export default function Home() {
