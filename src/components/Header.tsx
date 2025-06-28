4ae764d7f690235bf7ac8d7b2bf47db6eafeb2d3
nexarithm <nexarithm@gmail.com>
2025-06-28 19:38:45 -0400

    feat: close mobile nav on link click
    This commit modifies the  component to automatically close the mobile navigation menu when a navigation link is clicked. This improves the user experience on mobile devices by preventing the menu from obscuring the content after a selection has been made.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,14 @@ import { useState } from 'react';
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div className="flex justify-center items-center bg-gray-800">
       <header className="w-full max-w-[1200px] p-2 flex flex-wrap justify-between items-center">
-        <Link to="/" className="flex items-center">
+        <Link to="/" className="flex items-center" onClick={handleLinkClick}>
           <div className="flex items-center">
             <img src="/Nexarithm_Avatar.webp" alt="Logo" className="w-12 h-12 md:w-16 md:h-16 rounded-full" />
             <span className="text-white text-lg md:text-xl font-bold ml-3">/ Nexarithm</span>
@@ -30,14 +34,14 @@ export default function Header() {
 
         {/* Navigation Links */}
         <div className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex w-full md:w-auto flex-col md:flex-row items-center mt-4 md:mt-0 space-y-2 md:space-y-0 md:space-x-6`}>
-          <Link to="/blogs" className="text-white hover:text-gray-300 block">BLOGS</Link>
-          <Link to="/projects" className="text-white hover:text-gray-300 block">PROJECTS</Link>
-          <Link to="/about" className="text-white hover:text-gray-300 block">ABOUT</Link>
+          <Link to="/blogs" className="text-white hover:text-gray-300 block" onClick={handleLinkClick}>BLOGS</Link>
+          <Link to="/projects" className="text-white hover:text-gray-300 block" onClick={handleLinkClick}>PROJECTS</Link>
+          <Link to="/about" className="text-white hover:text-gray-300 block" onClick={handleLinkClick}>ABOUT</Link>
         </div>
 
         <div className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex w-full md:w-auto flex-col md:flex-row items-center mt-4 md:mt-0 space-y-2 md:space-y-0 md:space-x-6`}>
-          <a href="https://x.com/nexarithm" className="text-white hover:text-gray-300 block">Twitter</a>
-          <a href="https://github.com/Nexarithm" className="text-white hover:text-gray-300 block">GitHub</a>
+          <a href="https://x.com/nexarithm" className="text-white hover:text-gray-300 block" onClick={handleLinkClick}>Twitter</a>
+          <a href="https://github.com/Nexarithm" className="text-white hover:text-gray-300 block" onClick={handleLinkClick}>GitHub</a>
         </div>
       </header>
     </div>
