fbfe231c8b14b36d71efb568929b0b578f907d0f
nexarithm <nexarithm@gmail.com>
2025-07-02 13:50:54 -0700

    feat: add profile picture to about and home pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,11 +13,19 @@ export const AboutSection: FC = () => {
   return (
     <div className="w-full max-w-[1200px] px-4">
       <div className="flex flex-col items-center w-full">
-        <img
-          src="/Nexarithm_Avatar.webp"
-          alt="Nexarithm Avatar"
-          className="max-w-[300px] h-auto mb-5 rounded-xl w-[300px]"
-        />
+        <div className="flex items-center justify-center mb-5">
+          <img
+            src="/profile_pic.jpg"
+            alt="Osman Aka"
+            className="max-w-[250px] h-auto rounded-xl w-[250px]"
+          />
+          <span className="text-4xl font-bold mx-4">/</span>
+          <img
+            src="/Nexarithm_Avatar.webp"
+            alt="Nexarithm Avatar"
+            className="max-w-[250px] h-auto rounded-xl w-[250px]"
+          />
+        </div>
         <h1 className="text-2xl font-bold">Hello World!</h1>
       </div>
 
@@ -25,16 +33,18 @@ export const AboutSection: FC = () => {
         <p className="text-lg text-right leading-loose">
           I'm Nexarithm, and this is my digital playground where I share my half-baked thoughts on random things. <br/>
           <br/>
-          I have been hacking things for a long time. <br/>
-          During the day, I work in the Artificial Intelligence research realm. <br/>
-          At night, I work on my own projects.
+          <b>Employer:</b> Top Tier AI Research Lab, Silicon Valley<br/>
+          <b>Title:</b> Research Software Engineer<br/>
+          <b>Location:</b> New York / San Francisco<br/>
+          <b>Before:</b> Math and Computer Olympiad Medalist<br/>
+          <b>Main Indie Project:</b> <a href="https://www.proxai.co" target="_blank" rel="noopener noreferrer">ProxAI.co</a><br/>
         </p>
       </div>
 
       {/* Social Media Icons */}
       <div className="w-full flex flex-col justify-center items-center md:items-end gap-2 mt-4">
         <SocialLink href="https://x.com/nexarithm" icon={<TwitterIcon />} text="Twitter" />
-        <SocialLink href="https://www.proxai.co/blog" icon={<BlogIcon />} text="Blog" />
+        <SocialLink href="https://www.proxai.co/blog" icon={<BlogIcon />} text="ProxAI Blog" />
         <SocialLink href="https://github.com/Nexarithm" icon={<GitHubIcon />} text="GitHub" />
         <SocialLink href="https://news.ycombinator.com/user?id=nexarithm" icon={<HackerNewsIcon />} text="Hacker News" />
         <SocialLink href="https://www.reddit.com/user/InternationalToe5857" icon={<RedditIcon />} text="Reddit" />
